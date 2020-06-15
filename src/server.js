4762aa6c4c1de63ea1c8125728c0bc87886e66ab
ElverArevalo <elver.arevalo@gmail.com>
2020-06-15 12:11:40 -0500

    =cambios para heroku4

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/catalogoWeb'));
+app.use(express.static(__dirname+'/dist/catalogo-web'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/catalogoWeb/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/catalogo-web/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
