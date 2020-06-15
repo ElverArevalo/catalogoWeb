cb8eff16c46b5f22c95d25ac3b9e902881bb64ca
ElverArevalo <elver.arevalo@gmail.com>
2020-06-15 16:07:31 -0500

    =prueba heroku10

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/angularapp'));
  
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
  
 res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
